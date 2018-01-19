fa23d13ad53794301ec848a1b6d0bc101f069aa5
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-01-19 14:05:14 +0900

    housekeeping

--- bash_history_user.sh
+++ bash_history_user.sh
@@ -1,6 +1,7 @@
 python3 setup.py bdist_wheel
 python3 setup.py sdist
-sudo python3 pip install dist/*.whl
+sudo python3 -m pip install dist/*.whl
+python3 -m pip install --user dist/*.whl
 python3 -m ipython
 python3 -m jupyter notebook --ip=0.0.0.0 --port=8888
 python3 -m jupyter notebook --ip="$(hostname -i)" --port=8888
